7b2c51247c067e8073860167e29794babb4d342a
vig2306 <vignesh.sunder@gmail.com>
2020-05-24 21:24:00 +0530

    changes to tkinter

--- connect-4_tkinter.py
+++ connect-4_tkinter.py
@@ -5,11 +5,11 @@ from PIL import ImageTk, Image
 
 import os
 def call_window_Two_player():
-    os.system("python game.py")
+    os.system("python3 game.py")
     
 
 def call_window_One_player():
-    os.system("python connect4_with_ai.py")
+    os.system("python3 connect4_with_ai.py")
 
 def call_window_tkinterpage():
     root = Tk()
@@ -40,7 +40,7 @@ def call_window_tkinterpage():
 
 root = Tk()
 root.title("Connect-4")
-root.geometry("460x460")
+root.geometry("600x460")
 
 root.style = Style() 
 root.style.configure('W.TButton', font = 
@@ -55,23 +55,22 @@ frame.pack()
 imgpath = 'tkinter_1.jpg'
 img = Image.open(imgpath)
 photo = ImageTk.PhotoImage(img)
-canvas = Canvas(root,width=460,height=460)
-canvas.create_image(0,0,anchor=NW,image=photo)
-canvas.pack()
+canvas = Canvas(root,width=500,height=500)
+canvas.create_image(525,250,anchor=E,image=photo)
+canvas.pack(side="right")
 
-
-w = Label(root, text="Connect-4", font="none 24 bold", width=700, anchor=CENTER)
-w.place(relx=0.5, rely=0.1, anchor=CENTER)
+w = Label(root, text="Connect-4", font="none 24 bold", width=70)
+w.place(relx = 0.1,rely=0.1, anchor=W)
 
 
 button1 = Button(root, text="ONE PLAYER", width=20, command= call_window_One_player)
-button1.place(relx=0.5, rely=0.3, anchor=CENTER)
+button1.place(relx =0.1, rely=0.3, anchor=W)
 
 button2 = Button(root, text="TWO PLAYER", width=20,command= call_window_tkinterpage)
-button2.place(relx=0.5, rely=0.45, anchor=CENTER)
+button2.place(relx=0.1, rely=0.45, anchor=W)
 
 button3 = Button(root, text="EXIT", width=20, command= frame.quit)
-button3.place(relx=0.5, rely=0.6, anchor=CENTER)
+button3.place(relx=0.1, rely=0.6, anchor=W)
 
 
         
